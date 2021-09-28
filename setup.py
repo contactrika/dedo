4c422fe43dd06792faf9c58ccd6f805e5262c118
contactrika <contact.rika@gmail.com>
2021-09-28 11:54:02 -0700

    robot cleanup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='dedo',
       packages=['dedo'],
       install_requires=[
             'numpy', 'gym==0.20.0', 'pybullet==3.1.7',
-            'torch', 'stable_baselines3=1.2.0',
+            'torch', 'stable_baselines3==1.2.0',
             'matplotlib', 'tensorboard', 'tensorboardX', 'moviepy', 'wandb',
             'pyaml', 'opencv-python',
       ])
